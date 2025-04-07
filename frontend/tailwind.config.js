9179961fed12ecbba412255e77f29f49089233dc
josedonate <garciamarinjr2003@gmail.com>
2025-04-07 12:26:10 +0200

    Eliminación carpeta frontend por problemas de configuración

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-      "./src/app/**/*.{js,ts,jsx,tsx}",  // Ruta correcta para App Router
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
